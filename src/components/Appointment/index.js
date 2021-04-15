34bba49efe59d1898f0ffe4cdecfe12686211ea3
ban-mazin <ban.qaddoori89@gmail.com>
2021-04-15 16:21:36 -0600

    use mode in appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,25 +5,43 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
+import useVisualMode from "../../hooks/useVisualMode";
+
+
 
 
 
 export default function Appointment (props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const DELETEING = "DELETEING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   
   return (
   <article className="appointment" data-testid="appointment">
   <Header
   time={props.time}
   />
-  {props.interviewer ? 
+  
 
+  {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+{mode === SHOW && (
   <Show
     student={props.interview.student}
     interviewer={props.interview.interviewer}
-    
+    onDelete={() => transition(CONFIRM)}
+        onEdit={() => transition(EDIT)}
   />
-  : <Empty/>
-}
+)}
+   {/* <Empty/> */}
+
+
 
   </article>
   ) 
